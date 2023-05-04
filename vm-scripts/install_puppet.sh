8387062175cf9f8e7abf7e1b4258907991cfaa2b
Bert Hajee <bert.hajee@enterprisemodules.com>
2023-05-04 11:57:22 +0200

    Start using puppet 8

--- vm-scripts/install_puppet.sh
+++ vm-scripts/install_puppet.sh
@@ -12,7 +12,8 @@ else
   fi
   echo "Installing $PACKAGE"
   rhel=$(awk -F'[ .]' '{if ($1=="AlmaLinux") print $3; else if (NF==8) print $6; else if (NF==9) print $7}' /etc/redhat-release)
-  yum install -y --nogpgcheck https://yum.puppetlabs.com/puppet-release-el-${rhel}.noarch.rpm > /dev/null
+  yum install -y --nogpgcheck https://yum.puppetlabs.com/puppet8-release-el-${rhel}.noarch.rpm > /dev/null
+  # yum install -y --nogpgcheck https://yum.puppetlabs.com/puppet7-release-el-${rhel}.noarch.rpm > /dev/null
   yum install -y --nogpgcheck $PACKAGE
   rpm -q git || yum install -y --nogpgcheck git
 
