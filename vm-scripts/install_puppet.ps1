eb9d39aedfcb9c01004acad18998192d0c8c736f
Remy van Berkum <r.van.berkum@enterprisemodules.com>
2024-07-04 18:50:14 +0200

    Add puppetlabs-host_core module and Precompile .NET assemblies on Windows

--- vm-scripts/install_puppet.ps1
+++ vm-scripts/install_puppet.ps1
@@ -3,20 +3,25 @@ if ([System.IO.File]::Exists("$Env:LOCALAPPDATA\install_puppet.done")) {
 } else {
     Write-Output 'Disable Security scanning...'
     Set-MpPreference -DisableArchiveScanning $true -DisableRealtimeMonitoring $true
+
+    Write-Output 'Downloading Puppet Agent...'
     $uri = "https://downloads.puppetlabs.com/windows/puppet8/puppet-agent-x64-latest.msi"
     $out = "c:\windows\temp\puppet-agent-x64-latest.msi"
-    Write-Output 'Downloading Puppet Agent...'
     Invoke-WebRequest -Uri $uri -OutFile $out
+
     Write-Output 'Installing Puppet Agent...'
     Start-Process C:\Windows\System32\msiexec.exe -ArgumentList "/qn /norestart /i $out" -wait
     Write-Output 'Puppet installed'
+
     Write-Output 'Installing Chocolatey'
     Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
     Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
     Write-Output 'Chocolatey installed'
-    Write-Output 'Installing git'
     $env:PATH += ';C:\ProgramData\chocolatey'
+
+    Write-Output 'Installing git'
     Invoke-Expression "& choco install git -y --no-progress"
     Write-Output 'git installed'
+
     New-Item -ItemType file $Env:LOCALAPPDATA\install_puppet.done
 }
