8f70ce7b15a0da8e7c4886de144b6f3d16b36306
Bert Hajee <bert.hajee@enterprisemodules.com>
2025-02-18 15:47:54 +0100

    Start using EM Vagrant plugin

--- vm-scripts/install_modules.ps1
+++
@@ -1,9 +0,0 @@
-if ([System.IO.File]::Exists("$Env:LOCALAPPDATA\install_modules.done")) {
-    Write-Output "Modules already installed"
-} else {
-    Write-Output 'Installing required puppet modules...'
-    Set-Location 'c:\vagrant'
-    Invoke-Expression "& 'c:\Program Files\Puppet Labs\Puppet\puppet\bin\r10k.bat' puppetfile install --verbose"
-    Write-Output 'Installing required puppet modules finished.'
-    New-Item -ItemType file $Env:LOCALAPPDATA\install_modules.done
-}
--- vm-scripts/run_puppet.ps1
+++
@@ -1,2 +0,0 @@
-Write-Output 'The Windows server has been setup.'
-Write-Output "Now run 'puppet apply C:\vagrant\manifests\site.pp -t' from inside the VM (this doesn't work using vagrant)"
