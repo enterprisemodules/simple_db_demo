a6353808f5421b34f2bf9f7e420cc34af578f37c
Remy van Berkum <remy.van.berkum@enterprisemodules.com>
2025-11-06 13:49:48 +0100

    [quality] Use puppet-swap_file module and make pipelines more robust (#34)
    Change-Id: I97b43b3f704e00ee358076eebe787b13de52efb5

--- modules/profile/manifests/base/vagrant.pp
+++ modules/profile/manifests/base/vagrant.pp
@@ -8,21 +8,10 @@ class profile::base::vagrant {
 
     ensure_resources('package', $required_packages)
 
-    exec { 'create swap file':
-      command => '/bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=8192',
-      creates => '/var/swap.1',
-    }
-
-    -> exec { 'attach swap file':
-      command => '/sbin/mkswap /var/swap.1 && /sbin/swapon /var/swap.1',
-      unless  => '/sbin/swapon -s | grep /var/swap.1',
-    }
-
-    #add swap file entry to fstab
-    -> exec { 'add swapfile entry to fstab':
-      command => '/bin/echo >>/etc/fstab /var/swap.1 swap swap defaults 0 0',
-      user    => root,
-      unless  => "/bin/grep '^/var/swap.1' /etc/fstab 2>/dev/null",
+    swap_file::files { '8GB Swap':
+      ensure       => present,
+      swapfile     => '/var/swap.8gb',
+      swapfilesize => '8GB',
     }
   } elsif $facts['kernel'] == 'Windows' {
     # For performance reasons in VirtualBox
